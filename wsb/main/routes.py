aed7bc976228750c051524858027b6d423f0f601
andyvandy <vandenhoeven.andrew@gmail.com>
2021-01-29 00:14:53 -0500

    WIP

--- wsb/main/routes.py
+++ wsb/main/routes.py
@@ -7,5 +7,5 @@ main = Blueprint("main", __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-  threads = Thread.query.all()
-  return render_template("main/index.html", threads=threads, title="Home")
+  # threads = Post.query.all()
+  return render_template("main/index.html", threads=[], title="Home")
