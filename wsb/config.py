704ef2c93ad4cb94d0c115276491715879657e4e
andyvandy <vandenhoeven.andrew@gmail.com>
2021-01-29 17:05:46 -0500

    make print to console a command line arg

--- wsb/config.py
+++ wsb/config.py
@@ -2,13 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-  SECRET_KEY = "W4dNaPqRXjW1eZG2d9vXW4dNaPqRXjW1eZG2d9vX"
+    SECRET_KEY = "W4dNaPqRXjW1eZG2d9vXW4dNaPqRXjW1eZG2d9vX"
 
-  REDDIT_CREDENTIALS = {
-    'username':'wsb_api_throwaway',
-    'password':'Hunter2',
-    'client_id':'xXq6FWhj3Q7dSg',
-    'client_secret': 'pehEpglhVFg2MNFByjzwLXmLv6ha1A',
-    'user_agent':'testscript by u/fakebot3 but not really i just copy pasted' }
+    REDDIT_CREDENTIALS = {
+        'username':'wsb_api_throwaway',
+        'password':'Hunter2',
+        'client_id':'xXq6FWhj3Q7dSg',
+        'client_secret': 'pehEpglhVFg2MNFByjzwLXmLv6ha1A',
+        'user_agent':'testscript by u/fakebot3 but not really i just copy pasted' }
 
-  SOCKETS = False
+    SOCKETS = True
