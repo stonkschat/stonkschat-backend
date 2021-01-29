ee38d66e00ada84ce8fec826cb423572e778fa31
Ian Koide <ian.koide@protonmail.com>
2021-01-28 20:30:15 -0500

    post model -> thread model

--- wsb/main/routes.py
+++ wsb/main/routes.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
-from wsb.models import Post
+from wsb.models import Thread
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 @main.route("/home")
 def home():
-  posts = Post.query.all()
-  return render_template("main/index.html", posts=posts, title="Home")
+  threads = Post.query.all()
+  return render_template("main/index.html", threads=threads, title="Home")
