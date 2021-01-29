57100473357003cff4c243941eee04c23e114018
andyvandy <vandenhoeven.andrew@gmail.com>
2021-01-29 11:22:44 -0500

    cleanup the POC a bit

---
+++ wsb/ReadMe.md
@@ -0,0 +1,5 @@
+# Steps to run locally
+
+1. `pipenv install`
+2. `pipenv run run.py`
+3. open http://127.0.0.1:5000/
--- wsb/scripts/redcreds.py
+++
@@ -1,5 +0,0 @@
-creds = { 'username':'USERNAME',
-    'password':'PASS',
-    'client_id':'XmGgOMtR6O6xRA',
-    'client_secret': 'gaDXegIyt1nxuXaAEfsC8vE6zssnPg',
-    'user_agent':'testscript by u/fakebot3 but not really i just copy pasted'}
